98e791bd43a3856afa74ee5efe62ac189496b99a
anMois <kew1672@naver.com>
2024-08-13 17:47:44 +0900

    [Create] Interaction 인터페이스 생성
    상호작용 가능한 인터페이스 생성

---
+++ TextRPG/Interfaces/IInteractable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPG.Players;
+
+namespace TextRPG.Interfaces
+{
+    public interface IInteractable
+    {
+        public void Interaction(Player player);
+    }
+}
